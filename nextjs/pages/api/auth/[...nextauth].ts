352efa13c20c0b0933dff9c14c56d3a3218991b7
michaelyocca <michael.yocca@geopointe.com>
2021-11-18 09:53:13 -0800

    added nextauth package and upgraded to nextjs 12

---
+++ nextjs/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth";
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.Cognito({
+      clientId: process.env.COGNITO_CLIENT_ID,
+      domain: process.env.COGNITO_DOMAIN
+    })
+  ]
+})
