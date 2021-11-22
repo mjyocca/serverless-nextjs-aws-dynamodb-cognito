83cb95c3a0104445476b89afc61c5b7c47156cc6
michaelyocca <michael.yocca@geopointe.com>
2021-11-22 10:51:40 -0800

    implement sign in with next-auth

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Provider } from 'next-auth/client';
+import { GeistProvider, CssBaseline, Page } from '@geist-ui/react';
+import { SWRConfig } from 'swr';
+import fetcher from '../lib/fetcher';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Provider session={pageProps.session}>
+        <Head><title>React Blog with NextJS & DynamoDB</title></Head>
+        <SWRConfig value={{ fetcher }}>
+          <GeistProvider>
+            <CssBaseline />
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </GeistProvider>
+        </SWRConfig>
+      </Provider>
+    </>
+  )
 }
-export default MyApp
--- nextjs/pages/api/auth/[...nextauth].ts
+++ nextjs/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
 import NextAuth from "next-auth";
-import Providers from 'next-auth/providers';
+import Providers from "next-auth/providers";
 
 export default NextAuth({
   providers: [
     Providers.Cognito({
       clientId: process.env.COGNITO_CLIENT_ID,
-      domain: process.env.COGNITO_DOMAIN
+      clientSecret: process.env.COGNITO_CLIENT_SECRET,
+      domain: process.env.COGNITO_DOMAIN,
     })
   ]
 })
